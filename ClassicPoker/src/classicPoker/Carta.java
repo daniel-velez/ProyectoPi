771b4cffea972ca4d955f3705ddd7e4a85833d09
Jaoe98 <julianorejuela@hotmail.com>
2020-10-17 17:31:29 -0500

    carta last modified

--- ClassicPoker/src/classicPoker/Carta.java
+++ ClassicPoker/src/classicPoker/Carta.java
@@ -1,20 +1,12 @@
 package classicPoker;
 
 public class Carta {
-    private int numero;
-    private Palos palo;
+    public final int numero;
+    public final Palos palo;
 
     Carta(int numero, Palos palo) {
         this.numero = numero;
         this.palo = palo;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public Palos getPalo() {
-        return palo;
     }
 }
 
